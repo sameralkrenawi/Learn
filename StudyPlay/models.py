9d686e6f8a4b4c54484d3806ad14e0b02c44940d
Eytan12 <58141479+Eytan12@users.noreply.github.com>
2021-04-12 00:52:39 +0300

    Update models.py

--- StudyPlay/models.py
+++ StudyPlay/models.py
@@ -4,8 +4,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
-
-class AdminModel(models.Model):
+class AdminModel(AbstractBaseUser,models.Model):
     ID = models.IntegerField(db_column='ID',primary_key=True)  # Field name made lowercase.
     Adminid = models.CharField(db_column='Adminid', max_length=9)  # Field name made lowercase.
     Pseudo= models.CharField(db_column='Pseudo',max_length=10)
@@ -16,7 +15,7 @@ class AdminModel(models.Model):
         db_table = 'admin'
 
 
-class ChildModel(models.Model):
+class ChildModel(AbstractBaseUser,models.Model):
     Pseudo= models.CharField(db_column='Pseudo',max_length=10)
     Password = models.CharField(db_column='Password',max_length=100)
     Email= models.TextField(db_column='Email')
@@ -25,7 +24,7 @@ class ChildModel(models.Model):
         db_table = 'Child'
 
 
-class WorkersModel(models.Model):
+class WorkersModel(AbstractBaseUser,models.Model):
     ID = models.IntegerField(db_column='ID',primary_key=True)  # Field name made lowercase.
     Workerid= models.CharField(db_column='Workerid',max_length=10)
     Pseudo= models.CharField(db_column='Pseudo',max_length=10)
@@ -37,7 +36,7 @@ class WorkersModel(models.Model):
         db_table = 'workers'
 
 
-class ParentsModel(models.Model):
+class ParentsModel(AbstractBaseUser,models.Model):
     Pseudo= models.CharField(db_column='Pseudo',max_length=10)
     Password = models.CharField(db_column='Password',max_length=100)
     Email= models.TextField(db_column='Email')
@@ -45,7 +44,7 @@ class ParentsModel(models.Model):
         managed = True
         db_table = 'Parents'
 
-class ActivitiesModel(models.Model):
+class ActivitiesModel(AbstractBaseUser,models.Model):
     Name = models.CharField(db_column='Name',max_length=100)
     Subject = models.CharField(db_column='Subject',max_length=100)
     class Meta:
