d872a31ac1f1a979ef1906eaaf6cda9a2ac5215d
DavidTe2 <davidte2@ac.sce.ac.il>
2021-04-13 16:42:28 +0300

    UserStory112-109-97

--- StudyPlay/models.py
+++ StudyPlay/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
-class AdminModel(AbstractBaseUser,models.Model):
+class AdminModel(models.Model):
     ID = models.IntegerField(db_column='ID',primary_key=True)  # Field name made lowercase.
     Adminid = models.CharField(db_column='Adminid', max_length=9)  # Field name made lowercase.
     Pseudo= models.CharField(db_column='Pseudo',max_length=10)
@@ -15,16 +15,20 @@ class AdminModel(AbstractBaseUser,models.Model):
         db_table = 'admin'
 
 
-class ChildModel(AbstractBaseUser,models.Model):
+class ChildModel(models.Model):    
+    ID = models.IntegerField(db_column='ID',primary_key=True)  # Field name made lowercase.
     Pseudo= models.CharField(db_column='Pseudo',max_length=10)
     Password = models.CharField(db_column='Password',max_length=100)
+    Age= models.TextField(db_column='Age')
     Email= models.TextField(db_column='Email')
+    ParentsPseudo= models.CharField(db_column='ParentsPseudo',max_length=100)
     class Meta:
         managed = True
-        db_table = 'Child'
+        db_table = 'child'
 
 
-class WorkersModel(AbstractBaseUser,models.Model):
+class WorkersModel(models.Model):
+    
     ID = models.IntegerField(db_column='ID',primary_key=True)  # Field name made lowercase.
     Workerid= models.CharField(db_column='Workerid',max_length=10)
     Pseudo= models.CharField(db_column='Pseudo',max_length=10)
@@ -36,7 +40,8 @@ class WorkersModel(AbstractBaseUser,models.Model):
         db_table = 'workers'
 
 
-class ParentsModel(AbstractBaseUser,models.Model):
+class ParentsModel(models.Model):
+    ID = models.IntegerField(db_column='ID',primary_key=True)  # Field name made lowercase.
     Pseudo= models.CharField(db_column='Pseudo',max_length=10)
     Password = models.CharField(db_column='Password',max_length=100)
     Email= models.TextField(db_column='Email')
@@ -44,7 +49,7 @@ class ParentsModel(AbstractBaseUser,models.Model):
         managed = True
         db_table = 'Parents'
 
-class ActivitiesModel(AbstractBaseUser,models.Model):
+class ActivitiesModel(models.Model):
     Name = models.CharField(db_column='Name',max_length=100)
     Subject = models.CharField(db_column='Subject',max_length=100)
     class Meta:
