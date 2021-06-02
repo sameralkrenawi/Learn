28a2c91e1673a45129c51711d332ec6c1d5fc6f0
DavidTe2 <davidte2@ac.sce.ac.il>
2021-06-02 09:11:45 +0300

    BS_PM2021_TEAM_24 As a parent, I would like to see my child's progress, so that i can follow his achievements

--- StudyPlay/models.py
+++ StudyPlay/models.py
@@ -15,14 +15,14 @@ class AdminModel(models.Model):
         db_table = 'admin'
 
 
-class ChildModel(models.Model):    
+class ChildModel(models.Model):
     ID = models.IntegerField(db_column='ID',primary_key=True)  # Field name made lowercase.
     Pseudo= models.CharField(db_column='Pseudo',max_length=10)
-    Password = models.CharField(db_column='Password',max_length=100)
+    Password = models.CharField(db_column='Password',max_length=10)
     Age= models.TextField(db_column='Age')
     Email= models.TextField(db_column='Email')
-    profile_pic = models.ImageField(u"profile1.png",blank=True,upload_to="static/images/")
-    ParentsPseudo= models.CharField(db_column='ParentsPseudo',max_length=100)
+    ParentsPseudo= models.CharField(db_column='ParentsPseudo',default=None,max_length=10)
+    profile_pic = models.ImageField(u"profile1.png",blank=True,default=None,upload_to="static/images/")
     class Meta:
         managed = True
         db_table = 'child'
@@ -46,42 +46,42 @@ class ParentsModel(models.Model):
     Password = models.CharField(db_column='Password',max_length=100)
     Email= models.TextField(db_column='Email')
     country= models.TextField(db_column='country')
-    profile_pic = models.ImageField(u"profile1.png",blank=True,upload_to="static/images/")
+    profile_pic = models.ImageField(u"profile1.png",blank=True,default=None,upload_to="static/images/")
     class Meta:
         managed = True
         db_table = 'Parents'
 
 class ActivitiesModel(models.Model):
-    ID = models.IntegerField(db_column='ID',primary_key=True) 
+    ID = models.IntegerField(db_column='ID',default=None,primary_key=True) 
     Name = models.CharField(db_column='Name',max_length=100)
     Subject = models.CharField(db_column='Subject',max_length=100)
-    Link = models.CharField(db_column='Link',max_length=100)
+    Link = models.CharField(db_column='Link',default=None,max_length=100)
     class Meta:
         managed = True
         db_table = 'activities'
 
 class CountriesModel(models.Model):
-    ID = models.IntegerField(db_column='ID',primary_key=True) 
-    Name = models.CharField(db_column='Name',max_length=100)
+    ID = models.IntegerField(db_column='ID',default=None,primary_key=True) 
+    Name = models.CharField(db_column='Name',default=None,max_length=100)
     Count = models.IntegerField(db_column='Count',default=0)
     class Meta:
         managed= True
         db_table='countries'
 
 class ReviewsModel(models.Model):
-    ID = models.IntegerField(db_column='ID',primary_key=True) 
-    Description = models.CharField(db_column='Description',max_length=100)
+    ID = models.IntegerField(db_column='ID',default=None,primary_key=True) 
+    Description = models.CharField(db_column='Description',default=None,max_length=100)
     Ratings = models.IntegerField(db_column='Ratings')
     class Meta:
         managed= True
         db_table='reviews'
 
 class ActivityDoneModel(models.Model):
-    ID = models.IntegerField(db_column='ID',primary_key=True) 
-    NameAct = models.CharField(db_column='NameAct',max_length=100)
-    PseudoC = models.CharField(db_column='PseudoC',max_length=100)
-    PseudoP = models.CharField(db_column='PseudoP',max_length=100)
-    Grade = models.CharField(db_column='Grade',max_length=100)
+    ID = models.IntegerField(db_column='ID',default=None,primary_key=True) 
+    NameAct = models.CharField(db_column='NameAct',default=None,max_length=100)
+    PseudoC = models.CharField(db_column='PseudoC',default=None,max_length=100)
+    PseudoP = models.CharField(db_column='PseudoP',default=None,max_length=100)
+    Grade = models.CharField(db_column='Grade',default=None,max_length=100)
     class Meta:
         managed= True
         db_table='activityDone'
